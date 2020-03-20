be9e82e9f14c179b0861ac14f6c2cb83101474db
Nezz7 <omarkamoun97@gmail.com>
2020-03-20 12:39:33 +0100

    update Libarary

--- Library/SegTree.cpp
+++ Library/SegTree.cpp
@@ -1,42 +1,47 @@
-#include <bits/stdc++.h>
-using namespace std;
-int const MAXN  = 1e5 + 9;
-int tree[4*MAXN];
-int a[MAXN];
-void build (int node, int start, int end){
-    if (start == end){
-        tree[node] = a[start];
-        printf("tree[%d] = %d \n",node,tree[node]);
-        return;
+struct SegTree{
+    int tree[4*MAXN];
+    int a[MAXN];
+    const int MYZERO = ; //ZERO OF F
+    SegTree(vector<int>&v){
+        int n = v.size();
+        for(int i = 0; i < n; i++)
+            a[i] = v[i];
+        build(1,0,n-1);
     }
-    int mid = (start + end) / 2;
-    build(2*node,start,mid);
-    build(2*node+1,mid+1,end);
-    tree[node] = tree[2*node] + tree[2*node + 1];
-    printf("tree[%d] = %d \n",node,tree[node]);
-}
-int query (int node, int start, int end, int l, int r){
-    if (r < start or end < l) return 0;
-    if (l <= start  && end <= r) return tree[node];
-    int mid = (start + end) / 2;
-    int p1 = query(2*node,start,mid,l,r);
-    int p2 = query(2*node+1,mid+1,end,l,r);
-    return p1 + p2;
-}
-void update (int node, int start, int end, int idx, int val){
-    if (start == end){
-        tree[node] = val;
-        printf("tree[%d] = %d \n",node,tree[node]);
-        return;
+    int merge(int a, int b){ // DEFINE
+        return 
     }
-    int mid = (start + end) / 2;
-    if (idx <= mid){
-        update(2*node,start,mid,idx,val);
-    }else {
-         update(2*node  + 1,mid + 1, end,idx,val);
+    void build (int node, int start, int end){
+        if (start == end){
+            tree[node] = a[start];
+            return;
+        }
+        int mid = (start + end) / 2;
+        build(2*node,start,mid);
+        build(2*node+1,mid+1,end);
+        tree[node] = tree[2*node] + tree[2*node + 1];
     }
-    tree[node] = tree[2*node] + tree[2*node + 1];
-    printf("tree[%d] = %d \n",node,tree[node]);
+    int query (int node, int start, int end, int l, int r){
+        if (r < start or end < l) return MYZERO;
+        if (l <= start  && end <= r) return tree[node];
+        int mid = (start + end) / 2;
+        int p1 = query(2*node,start,mid,l,r);
+        int p2 = query(2*node+1,mid+1,end,l,r);
+        return merge(p1,p2);
+    }
+    void update (int node, int start, int end, int idx, int val){
+        if (start == end){
+            tree[node] = val;
+            return;
+        }
+        int mid = (start + end) / 2;
+        if (idx <= mid){
+            update(2*node,start,mid,idx,val);
+        }else {
+            update(2*node  + 1,mid + 1, end,idx,val);
+        }
+        tree[node] = tree[2*node] + tree[2*node + 1];
+    }
 }
 int main (){
     int n;
--- Library/orderStatistic.cpp
+++ Library/orderStatistic.cpp
@@ -1,12 +1,22 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
-
+#define f first
+#define s second
+#define endl '\n'
+#define LL long long
+#define pb push_back
+#define sz(a) (int)a.size()
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define debug(x) cerr << #x << " is " << x << endl;
 using namespace std;
 using namespace __gnu_pbds;
+int const MAXN = 2e6 + 9;
+
 
 typedef tree<int, null_type, less <int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 ordered_set  st;
-    int n, k, a , m;
+
 int get_next(int x){
     int i = st.order_of_key(x);
     auto it = st.find_by_order(i);
