9bc0c7ce62dbc551f1c42e090232bc5d539f4e55
Nezz7 <omarkamoun97@gmail.com>
2020-05-31 20:14:44 +0100

    # 646 div.2

--- Codeforces/#646 (Div. 2)/A.cpp
+++ Codeforces/#646 (Div. 2)/A.cpp
@@ -11,5 +11,23 @@ using namespace std;
 int const MAXN = 2e6 + 9;
 int main(){
     ios_base::sync_with_stdio (0),cin.tie(0);
-    
+    int t;
+    cin >> t;
+    while(t--){
+        int n, x;
+        cin >> n >> x;
+        int even = 0, odd = 0;
+        for(int i = 0; i < n; i++){
+            int a;
+            cin >> a;
+            if(a & 1) odd++;
+            else even++;
+        }
+        bool cond = false;
+        for(int i = 1; i <= x; i+=2){
+            if(odd >= i && even >= x - i) cond = true;
+        }
+        if(cond) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
 }
--- Codeforces/#646 (Div. 2)/C.cpp
+++ Codeforces/#646 (Div. 2)/C.cpp
@@ -8,8 +8,53 @@
 #define rall(a) a.rbegin(),a.rend()
 #define debug(x) cerr << #x << " is " << x << endl;
 using namespace std;
-int const MAXN = 2e6 + 9;
+int const MAXN = 2000 + 9;
+vector<int> g[MAXN];
+int sz[MAXN];
+string first = "Ayush";
+string second = "Ashish";
+void dfs(int node, int par){
+    sz[node] = 1;
+    for(auto child : g[node]){
+        if(par == child) continue;
+        dfs(child, node);
+        sz[node] += sz[child];
+    }
+}
 int main(){
     ios_base::sync_with_stdio (0),cin.tie(0);
-    
+    int t;
+    cin >> t;
+    while(t--){
+        int n, x;
+        cin >> n >> x;
+        for(int i = 1; i <= n; i++){
+            g[i].clear();
+            sz[i] = 0;
+        }
+        for(int i = 1; i < n; i++){
+            int u, v;
+            cin >> u >> v;
+            g[u].pb(v);
+            g[v].pb(u);
+        }
+        if(sz(g[x]) <= 1){
+            cout << first << endl;
+            continue;
+        } 
+        dfs(x,x);
+        bool cond = false;
+        for(auto a : g[x]){
+            for(auto b : g[x]){
+                    if(a == b) continue;
+                    int nbr = n - 1 - sz[a] - sz[b];
+                    int cur = sz[a] + sz[b] - 1;
+                    if((cur & 1) == (nbr & 1)){
+                        cond = true;
+                    }
+            }
+        }
+        if(cond) cout << first << endl;
+        else cout << second << endl;
+    }
 }
